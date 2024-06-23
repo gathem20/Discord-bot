6f10161c7ba949b25907f9b87ab917285e20adcf
Mohamed Tarek <140327283+Mohamed-Tarek11111@users.noreply.github.com>
2024-06-23 16:35:46 +0300

    Update index.ts mt

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import dotenv from "dotenv"
 import {Client , GatewayIntentBits} from "discord.js"
 
 dotenv.config()
-const client = new Client({
+const bot = new Client({
     intents:[
         GatewayIntentBits.Guilds , 
         GatewayIntentBits.GuildMessages,
@@ -10,5 +10,22 @@ const client = new Client({
         GatewayIntentBits.MessageContent
     ]
 })
+const prefix = "Bot-Do!";
 
-client.login(process.env.token)
+bot.on("ready", () => {
+    console.log("online");
+});
+
+bot.on("messageCreate", async (message) => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === "ping") {
+        message.reply("Pong");
+    }
+});
+
+bot.login(process.env.token)
