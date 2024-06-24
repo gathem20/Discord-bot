0713d036cea82864a24d4d58aa054af0d5d4fa2a
gathem <anoneg655@gmail.com>
2024-06-24 23:52:11 +0300

    clean code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
-import { Client, GatewayIntentBits } from "discord.js";
+import { Client, GatewayIntentBits, Message } from "discord.js";
+import { responses } from "./resposnes";
 import * as Dotenv from "dotenv";
+
 Dotenv.config();
 const token = process.env.token;
 const client = new Client({
@@ -13,16 +15,34 @@ const client = new Client({
 client.on("ready", (e) => {
   console.log("bot is ready");
 });
-
 client.on("messageCreate", (message: any) => {
-  console.log(message.content);
-  const content = message.content;
-  const parts = content.split(" ");
-  if (content.startsWith("!bot-do ping")) {
-    message.reply("pong");
-  } else if (parts[0] === "!bot-do") {
-    message.reply("i dont understand ?");
+  if (!message.content.startsWith("!bot-do ")) return;
+  const content = (
+    message.content.replace("!bot-do ", "") as string
+  ).toLowerCase();
+  let responded = false;
+  for (const response of responses) {
+    if (content === response.message) {
+      message.reply(response.response);
+      responded = true;
+    }
+    // for (const response of responses) {
+    //   if(content === response.message)
+    // }
   }
+  if (!responded) {
+    message.reply("I don't understand!");
+  }
+  console.log(content);
+  // const content = message.content;
+  // const parts = content.split(" ");
+  // if (content.startsWith("!bot-do ping")) {
+  //   message.reply("pong");
+  // } else if (parts[0] === "!bot-do") {
+  //   message.reply("i dont understand ?");
+  // }
+  // if (content === "!bot-do who are you") {
+  //   message.reply("i'm beta bot for multiverses");
+  // }
 });
-
 client.login(token);
---
+++ src/resposnes.ts
@@ -0,0 +1,12 @@
+import { Message } from "discord.js";
+
+export const responses = [
+  {
+    message: "ping",
+    response: "pong",
+  },
+  {
+    message: "who are you",
+    response: "I'm a beta bot for The Multiverse",
+  },
+];
