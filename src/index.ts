14db90af5142cc46d574f0b0071c7ec14ec50a97
Mohamed Tarek <140327283+Mohamed-Tarek11111@users.noreply.github.com>
2024-06-26 00:49:42 +0300

    Update mt

--- src/index.ts
+++ src/index.ts
@@ -12,37 +12,36 @@ const client = new Client({
     GatewayIntentBits.MessageContent,
   ],
 });
-client.on("ready", (e) => {
+client.on("ready", () => {
   console.log("bot is ready");
 });
-client.on("messageCreate", (message: any) => {
+
+client.on("messageCreate", (message: Message) => {
+  if (message.author.bot) return;
   if (!message.content.startsWith("!bot-do ")) return;
-  const content = (
-    message.content.replace("!bot-do ", "") as string
-  ).toLowerCase();
+
+  const content = message.content.slice("!bot-do ".length).trim().toLowerCase();
   let responded = false;
+
   for (const response of responses) {
-    if (content === response.message) {
-      message.reply(response.response);
-      responded = true;
+    if (Array.isArray(response.message)) {
+      if (response.message.some((msg) => content.startsWith(msg.toLowerCase()))) {
+        message.reply(response.response);
+        responded = true;
+        break;
+      }
+    } else {
+      if (content.startsWith(response.message.toLowerCase())) {
+        message.reply(response.response);
+        responded = true;
+        break;
+      }
     }
-    // for (const response of responses) {
-    //   if(content === response.message)
-    // }
   }
+
   if (!responded) {
     message.reply("I don't understand!");
   }
-  console.log(content);
-  // const content = message.content;
-  // const parts = content.split(" ");
-  // if (content.startsWith("!bot-do ping")) {
-  //   message.reply("pong");
-  // } else if (parts[0] === "!bot-do") {
-  //   message.reply("i dont understand ?");
-  // }
-  // if (content === "!bot-do who are you") {
-  //   message.reply("i'm beta bot for multiverses");
-  // }
 });
+
 client.login(token);
